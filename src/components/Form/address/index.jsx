516abae4c413ca44658c77eb4041f58cbe9fb4d9
Alex Boyce <alex.boyce@advisorsexcel.com>
2019-06-04 09:36:48 -0400

    Develop (#1)
    * Make address component more accessible
    
    * Fix dependency vulnerabilities
    
    * Add JetBrains IDE config files. Rebuild lib with dep updates

--- src/components/Form/address/index.jsx
+++ src/components/Form/address/index.jsx
@@ -3,30 +3,44 @@ import PropTypes from 'prop-types'
 import StateComboBox from '../combobox/states'
 import Input from '../input'
 
-const Address = ({name, required = false}) => {
+const Address = props => {
+  const {
+    name,
+    required = false,
+    containerClassName = '',
+    fieldClassName = 'slds-p-around-small',
+    streetFieldName = 'street',
+    streetLabel = 'Street Address',
+    cityFieldName = 'city',
+    cityLabel = 'City',
+    stateFieldName = 'state',
+    stateLabel = 'State',
+    postalCodeFieldName = 'postalCode',
+    postalCodeLabel = 'Postal Code'
+  } = props
   const sharedProps = {};
 
   if (required) {
     sharedProps.required = true;
   }
 
-  return <div>
-    <div className={`slds-p-around--small ${name}-street`}>
-      <Input name={`${name}.street`} label="Street Address" {...sharedProps}/>
+  return <div className={containerClassName}>
+    <div className={`${fieldClassName} ${name}-${streetFieldName}`}>
+      <Input name={`${name}.${streetFieldName}`} label={streetLabel} {...sharedProps}/>
     </div>
-    <div className={`slds-p-around--small ${name}-city`}>
-      <Input name={`${name}.city`} label="City" {...sharedProps}/>
+    <div className={`${fieldClassName} ${name}-${cityFieldName}`}>
+      <Input name={`${name}.${cityFieldName}`} label={cityLabel} {...sharedProps}/>
     </div>
     <div className="slds-grid">
-      <div className={`slds-grow slds-p-around--small ${name}-state`}>
-        <StateComboBox name={`${name}.state`}
-                       label="State"
+      <div className={`slds-grow ${fieldClassName} ${name}-${stateFieldName}`}>
+        <StateComboBox name={`${name}.${stateFieldName}`}
+                       label={stateLabel}
                        variant="inline-listbox"
                        {...sharedProps}
         />
       </div>
-      <div className={`slds-p-around--small ${name}-postal-code`}>
-        <Input name={`${name}.postalCode`} label="Postal Code" modelMask='*****-0000' {...sharedProps}/>
+      <div className={`${fieldClassName} ${name}-${postalCodeFieldName}`}>
+        <Input name={`${name}.${postalCodeFieldName}`} label={postalCodeLabel} modelMask='*****-0000' {...sharedProps}/>
       </div>
     </div>
   </div>
@@ -34,7 +48,17 @@ const Address = ({name, required = false}) => {
 
 Address.propTypes = {
   name: PropTypes.string.isRequired,
-  required: PropTypes.bool
+  required: PropTypes.bool,
+  containerClassName: PropTypes.string,
+  fieldClassName: PropTypes.string,
+  streetFieldName: PropTypes.string,
+  streetLabel: PropTypes.string,
+  cityFieldName: PropTypes.string,
+  cityLabel: PropTypes.string,
+  stateFieldName: PropTypes.string,
+  stateLabel: PropTypes.string,
+  postalCodeFieldName: PropTypes.string,
+  postalCodeLabel: PropTypes.string,
 }
 
 export default Address;
