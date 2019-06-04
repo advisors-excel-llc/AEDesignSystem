2180b62bd42b69998625682755f8635cf00055c9
curiosity26 <curiosity26@gmail.com>
2019-06-04 09:30:36 -0400

    Make address component more accessible

--- src/components/Form/address/__tests__/Address.test.js
+++ src/components/Form/address/__tests__/Address.test.js
@@ -14,7 +14,7 @@ describe('Address', function () {
 
     form.find('div.test-street input').simulate('change', {target: {value: 'Test Street'}})
     form.find('div.test-city input').simulate('change', {target: {value: 'Testville'}})
-    form.find('div.test-postal-code input').simulate('change', {target: {value: '11122'}})
+    form.find('div.test-postalCode input').simulate('change', {target: {value: '11122'}})
 
     form.find('div.test-state input').simulate('click')
     form.find('div.test-state span.slds-listbox__option').at(0).simulate('click')
@@ -25,5 +25,38 @@ describe('Address', function () {
       state: 'AL',
       street: 'Test Street',
     })
+  })
+
+  it('should support custom field names and labels', function () {
+    const form = mount(<Formik>
+      <Address name="test"
+               streetFieldName="billingStreet"
+               streetLabel="Billing Street"
+               cityFieldName="billingCity"
+               cityLabel="Billing City"
+               stateFieldName="billingState"
+               stateLabel="Billing State"
+               postalCodeFieldName="billingPostalCode"
+               postalCodeLabel="Billing Postal Code"
+      />
+    </Formik>)
+
+    form.find('div.test-billingStreet input').simulate('change', {target: {value: 'Test Street'}})
+    form.find('div.test-billingCity input').simulate('change', {target: {value: 'Testville'}})
+    form.find('div.test-billingPostalCode input').simulate('change', {target: {value: '11122'}})
+
+    form.find('div.test-billingState input').simulate('click')
+    form.find('div.test-billingState span.slds-listbox__option').at(0).simulate('click')
+
+    expect(getIn(form.state('values'), 'test')).toEqual({
+      billingCity: 'Testville',
+      billingPostalCode: '11122',
+      billingState: 'AL',
+      billingStreet: 'Test Street',
+    })
+    expect(form.find('div.test-billingStreet label').text()).toBe('Billing Street')
+    expect(form.find('div.test-billingCity label').text()).toBe('Billing City')
+    expect(form.find('div.test-billingState label').text()).toBe('Billing State')
+    expect(form.find('div.test-billingPostalCode label').text()).toBe('Billing Postal Code')
   })
 })
